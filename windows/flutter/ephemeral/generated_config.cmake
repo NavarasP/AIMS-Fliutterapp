d93ef681ca629b1e310a91c65dd53db97d9cee28
Navaras P <69687392+NavarasP@users.noreply.github.com>
2023-05-27 00:35:39 +0530

    Add existing project files to Git

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\ASUS\\Desktop\\Projects\\aims" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "PROJECT_DIR=C:\\Users\\ASUS\\Desktop\\Projects\\aims"
+  "FLUTTER_ROOT=C:\\src\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\ASUS\\Desktop\\Projects\\aims\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\ASUS\\Desktop\\Projects\\aims"
+  "FLUTTER_TARGET=C:\\Users\\ASUS\\Desktop\\Projects\\aims\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\ASUS\\Desktop\\Projects\\aims\\.dart_tool\\package_config.json"
+)
